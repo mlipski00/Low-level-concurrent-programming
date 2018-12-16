cc35584db5563764bc6d8d9a224b001e97ae5c31
mlipski00 <michal.lipski2@gmail.com>
2018-12-17 00:08:03 +0100

    Wyznaczenie środka symplexu

--- NonlinearOptimization.java
+++ NonlinearOptimization.java
@@ -1,37 +1,52 @@
 package com.company;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class NonlinearOptimization {
 
     final static int DIMENSION_SIZE = 5;
+    final static double EPSILON = 0.001;
 
     public static void main(String[] args) {
         System.out.println(ControlledRandomSearch(new int[]{0, 0, 0, 0, 0}));
         System.out.println(ControlledRandomSearch(new int[]{2, 0, 0, 0, 0}));
+        System.out.println("\nLosowe N punktów");
         Set<int[]> randomPoints = getRandomPoints();
-        for (int[] number : randomPoints) {
+        for (int[] point : randomPoints) {
             System.out.print("\n");
-            for (int valueOfPoint : number) {
+            for (int valueOfPoint : point) {
                 System.out.print(valueOfPoint + " | ");
             }
         }
-        System.out.print("\n");
-        System.out.println(getMinimalPoint(randomPoints));
+        System.out.println("\nLosowe 5 punktów");
+        Set<int[]> randomNPoints = getNPoints(randomPoints);
+        for (int[] point : randomNPoints) {
+            System.out.print("\n");
+            for (int valueOfPoint : point) {
+                System.out.print(valueOfPoint + " | ");
+            }
+        }
+        System.out.println("\n minimum i maksimum");
         System.out.println(getMaximalPoint(randomPoints));
+        double minimalPoint = getMinimalPoint(randomPoints);
+        System.out.println(minimalPoint);
+        System.out.println("\n Centrum symplexu");
+        double simplex = createSimplexAndGetCenter(randomNPoints, minimalPoint);
+
+            System.out.print(simplex + "\n | ");
     }
 
     static double ControlledRandomSearch(int[] point) {
         double sumOfXsqure = 0;
         double multiplicationOfCosXdividedByIterator = 0;
-        for (int i = 0; i < point.length; i++) {
-            // obliczenie sumy kwadratów wartości X tyle ile wymiarów
-            sumOfXsqure = +Math.sqrt(point[i]);
 
+        for (int i = 0; i < point.length; i++) {
+            // obliczenie sumy kwadratów wartości X dla każdej z wartości punku
+            if (point[i] != 0) {
+                sumOfXsqure = +point[i] * point[i];
+            }
         }
-        //obliczenie mnożenia cos(x/i) tyle ile wymiarów
+        //obliczenie mnożenia cos(x/i) dla każdej z wartości punku
         multiplicationOfCosXdividedByIterator = Math.cos(point[0]);
         for (int i = 1; i < point.length; i++) {
             multiplicationOfCosXdividedByIterator *= Math.cos(point[i]);
@@ -39,25 +54,24 @@ public class NonlinearOptimization {
         return 1 / 40 * sumOfXsqure + 1 - multiplicationOfCosXdividedByIterator;
     }
 
-    //krok 1
+    //krok 1 Wylosować zbiór P punktów o liczności N > 10(n + 1), gdzie n — wymiar zadania.
     static Set<int[]> getRandomPoints() {
         Set<int[]> generatedRandomPoints = new HashSet<>();
         Random random = new Random();
         // ograniczam maksymalną liczbę wylosowanych punktów do 100 + (10*(n+))
-        // dodatkowo dodaje 1 żeby liczba wylosowanych punków była większa od (10*(n+))
         int setOfPointsSize = random.nextInt(101) + 10 * (DIMENSION_SIZE + 1) + 1;
 
         while (generatedRandomPoints.size() != setOfPointsSize) {
             int[] point = new int[DIMENSION_SIZE];
             for (int i = 0; i < DIMENSION_SIZE; i++) {
-                point[i] = random.nextInt();
+                point[i] = random.nextInt(80) - 40;
             }
             generatedRandomPoints.add(point);
         }
         return generatedRandomPoints;
     }
 
-    //krok 2
+    //krok 2 wyliczanie mimalnej i maksymalnej wartości wśród każdego z wylosowanych punktów
     static double getMinimalPoint(Set<int[]> setOfRandomPoints) {
         double minimalPoint = Double.MAX_VALUE;
         double result;
@@ -80,5 +94,34 @@ public class NonlinearOptimization {
             }
         }
         return maximalPoint;
+    }
+
+    //krok 3 Wylosować ze zbioru P n punktów i utworzyć n + 1 wymiarowy sympleks. Wyznaczyć środek sympleksu:
+    static Set<int[]> getNPoints(Set<int[]> randomPoints) {
+        Random random = new Random();
+        List<int[]> listOfRandomPoints = new ArrayList<>(randomPoints);
+        int[] randomNPoints = new int[DIMENSION_SIZE];
+        Set<int[]> setOfNPoints = new HashSet<>();
+        for (int i = 0; i <= DIMENSION_SIZE; i++) {
+            setOfNPoints.add(listOfRandomPoints.get(random.nextInt(randomPoints.size())));
+        }
+        return setOfNPoints;
+    }
+
+    static double createSimplexAndGetCenter(Set<int[]> setOfNPoints, double bestPoint) {
+        List<int[]> listOfNPoints = new ArrayList<>(setOfNPoints);
+        double[] simplex = new double[DIMENSION_SIZE + 1];
+        simplex[0] = bestPoint;
+        for (int i = 1; i <= DIMENSION_SIZE; i++) {
+            int[] point = listOfNPoints.get(i - 1);
+            double crsResult = ControlledRandomSearch(point);
+            simplex[i] = crsResult;
+        }
+        double sumOfSimplexApexes = simplex[0];
+        for (int i = 1; i < DIMENSION_SIZE; i++) {
+            sumOfSimplexApexes =+ simplex[i];
+        }
+        double v = (1.0 / (double) DIMENSION_SIZE) * sumOfSimplexApexes;
+        return v;
     }
 }
